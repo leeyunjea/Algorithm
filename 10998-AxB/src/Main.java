b112bd650ba3cab6b2f6249172a951d811213383
leeyunjea <yunjea0312@naver.com>
2018-12-12 18:59:02 +0900

    Add 10998

---
+++ 10998-AxB/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str[] = br.readLine().split(" ");
+		
+		int a = Integer.parseInt(str[0]);
+		int b = Integer.parseInt(str[1]);
+		
+		bw.write(Integer.toString(a*b));
+		bw.flush();
+		bw.close();
+	}
+
+}
