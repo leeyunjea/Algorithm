b1b2b30fb5ad927e1ea1a9c0f883d78ae8e84360
leeyunjea <yunjea0312@naver.com>
2018-12-12 16:01:45 +0900

    Add first commit

---
+++ src/backjoon/helloworld/Main.java
@@ -0,0 +1,18 @@
+package backjoon.helloworld;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write("Hello World!");
+		bw.flush();
+		bw.close();
+	}
+
+}
