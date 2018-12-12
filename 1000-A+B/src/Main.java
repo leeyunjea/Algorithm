1e96eced5304b27374ebc9628c6dd3cf313378a3
iyunjae <yunjea0312@naver.com>
2018-12-12 16:58:24 +0900

    Add 1000-A+B main.java

--- 1000-A+B/src/Main.java
+++ 1000-A+B/src/Main.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String ab[] = br.readLine().split(" ");
+		
+		int a = Integer.parseInt(ab[0]);
+		int b = Integer.parseInt(ab[1]);
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = Integer.toString(a+b);
+		bw.write(str);
+		bw.flush();
+		bw.close();
 	}
 
 }
